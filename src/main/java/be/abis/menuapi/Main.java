05e59f96a979a0cefc03bb82d46fb7171ce01931
Clau Negrila <negrilaclaudia@gmail.com>
2022-11-09 15:55:23 +0100

    working Menu API

---
+++ src/main/java/be/abis/menuapi/Main.java
@@ -0,0 +1,16 @@
+package be.abis.menuapi;
+
+import be.abis.menuapi.model.Menu;
+import be.abis.menuapi.model.SandwichCompany;
+import be.abis.menuapi.service.AbisMenuService;
+import be.abis.menuapi.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
